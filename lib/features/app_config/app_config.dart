fbc9cf6031b674bc3f79b3ea38a1ceb6cc16de5e
Shinnakronkit Narkasen <shinnakronkit.nar@gmail.com>
2023-03-02 23:31:25 +0700

    delete unused

--- lib/features/app_config/app_config.dart
+++ lib/features/app_config/app_config.dart
@@ -45,43 +45,4 @@ class AppConfig {
         "api_url",
       );
   Uri get apiURI => Uri.parse(apiURL);
-
-  String get graphQLURL => const String.fromEnvironment(
-        "graph_ql_url",
-      );
-  Uri get graphQLURI => Uri.parse(graphQLURL);
-
-  String get graphQLV3URL => const String.fromEnvironment(
-        "graph_ql_v3_url",
-      );
-  Uri get graphQLV3URI => Uri.parse(graphQLV3URL);
-
-  String get graphQLApiKey => const String.fromEnvironment(
-        "graph_ql_key",
-      );
-
-  String get graphQLV3ApiKey => const String.fromEnvironment(
-        "graph_ql_v3_key",
-      );
-
-  String get webviewBaseURL => const String.fromEnvironment(
-        "webview_base_url",
-      );
-  Uri get webviewBaseURI => Uri.parse(webviewBaseURL);
-
-  String get dynamicLinkBaseURL => const String.fromEnvironment(
-        'dynamiclink_base_url',
-        defaultValue: 'https://dl.isuzu-tis.com',
-      );
-  Uri get dynamicLinkBaseURI => Uri.parse(dynamicLinkBaseURL);
-
-  String get firebaseDynamicLinkURL => const String.fromEnvironment(
-        'firebase_dynamiclink_url',
-        defaultValue: 'com.tis.myisuzu://google/link/',
-      );
-  Uri get firebaseDynamicLinkURI => Uri.parse(firebaseDynamicLinkURL);
-
-  String get lineChannel => const String.fromEnvironment(
-        "line_channel",
-      );
 }
